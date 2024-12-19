a694377daaec73a0ad340e9de3b62ab5dc38dedb
lmx <10632939+try-to-be-better@user.noreply.gitee.com>
2024-12-19 09:37:37 +0800

    lmx 20241219

---
+++ src/main/java/cn/lanqiao/testgit/LMX.java
@@ -0,0 +1,12 @@
+package cn.lanqiao.testgit;
+
+/**
+ * @ Author: 李某人
+ * @ Date: 2024/12/19/9:35
+ * @ Description:
+ */
+public class LMX {
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+}
