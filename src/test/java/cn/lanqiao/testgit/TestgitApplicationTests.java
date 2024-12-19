9e05871d80146fde9d1a53f7220619cbdb645abd
lmx <10632939+try-to-be-better@user.noreply.gitee.com>
2024-12-19 09:24:22 +0800

    first commit

---
+++ src/test/java/cn/lanqiao/testgit/TestgitApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lanqiao.testgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestgitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
